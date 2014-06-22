de65ec9170bf40ac4464472dc98bbcb80af57b31
Sam Whitehall <me@samwhitehall.com>
2014-06-23 00:30:54 +0100

    Improved model __repr__: includes all attributes

--- azscraper/model.py
+++ azscraper/model.py
@@ -11,7 +11,20 @@ class AmazonItem:
         self.top_review = self.get_top_review()
 
     def __repr__(self):
-        return self.name
+        return u'''
+        Name:   %s
+        Rating: %s
+        Price: %.2f GBP
+        Top Review:
+        %s ...
+        URL: %s
+        ''' % (
+            self.name,
+            '*' * int(round(self.rating)),
+            self.price,
+            self.top_review[:100].encode('ascii', 'ignore'),
+            self.url
+        )
     
     def get_name(self):
         return self.driver.find_element_by_css_selector('h1').text
@@ -21,12 +34,12 @@ class AmazonItem:
         rating = self.driver.find_element_by_css_selector(selector).\
             get_attribute('title')
         rating = rating[:3]
-        return rating
+        return float(rating)
 
     def get_price(self):
         selector = 'td#actualPriceContent b'
         price = self.driver.find_element_by_css_selector(selector).text[1:]
-        return price
+        return float(price)
 
     def get_top_review(self):
         top = self.driver.find_elements_by_class_name('reviews')
